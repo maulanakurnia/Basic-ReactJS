4becca1f79338451274da28996ba2f63658091e8
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-10 16:41:29 +0800

    Pemanggilan API (POST) #1

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -5,6 +5,12 @@ import axios from 'axios';
 class BlogPost extends Component {
     state = {
         post: [],
+        formBlogPost: {
+            id: 1,
+            title: '',
+            body: '',
+            userId: '',
+        }
     }
 
     getPostAPI = () => {
@@ -20,6 +26,18 @@ class BlogPost extends Component {
         axios.delete(`http://127.0.0.1:3000/posts/${data}`).then((res) => {
             this.getPostAPI();
         });
+    }
+
+    handleFormChange = (event) => {
+        // console.log(event.target);
+        let formBlogPostNew = {...this.state.formBlogPost};
+        // console.log(event.target.name);
+        formBlogPostNew[event.target.name] = event.target.value;
+        this.setState({
+            formBlogPost: formBlogPostNew
+        },() => {
+            console.log('value obj formBlogPost', this.state.formBlogPost)
+        })
     }
 
     componentDidMount(){
@@ -47,6 +65,13 @@ class BlogPost extends Component {
         return (  
             <Fragment>
                 <p className="section-title">Blog Post</p>
+                <div className="form-add-post">
+                    <label className="title">Title</label>
+                    <input type="text" name="title" className="title" placeholder="add title" onChange={this.handleFormChange}/>
+                    <label className="body">Blog Content</label>
+                    <textarea type="text" name="body" className="body-content" id="body" cols="30" rows="10" placeholder="add blog content" onChange={this.handleFormChange}/>
+                    <button className="btn-submit">Simpan</button>
+                </div>
                 {
                     this.state.post.map(post => {
                         return <Post key={post.id} data={post} remove={this.handleRemove} />
