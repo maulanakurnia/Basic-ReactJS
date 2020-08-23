5a3681c8656418db650146dca71fbee2eef48ef5
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-23 22:40:53 +0800

    Cleaning Up

--- src/component/StateLessComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './StateLessComponent.css'
-// Bisa Juga Dinamakan Funcitional Component
-const StateLessComponent = () => {
-    return <p className="text-p">Hello Functional component</p>
-}
-
-export default StateLessComponent;
--- src/container/StateFullComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class StateFullComponent extends React.Component{
-    render(){
-      return <p>Statefull Component!</p>
-    }
-}
-
-export default StateFullComponent;
