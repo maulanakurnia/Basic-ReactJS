9b8595109275f8212b9767e07ebcd5c21e14044d
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-23 23:38:20 +0800

    StateLess & StateFull Component

---
+++ src/container/Home/index.jsx
@@ -0,0 +1,38 @@
+import React, { Component, Fragment } from "react";
+import YoutubeComp from "../../component/YoutubeComp";
+
+class Home extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>Youtube Component</p>
+        <hr />
+        <YoutubeComp 
+            time="8.03" 
+            title="Tutorial React JS - Bagian 1" 
+            desc="14k ditonton, 12 hari yang lalu"/>
+        <YoutubeComp 
+            time="7.03" 
+            title="Tutorial React JS - Bagian 2" 
+            desc="20k ditonton, 10 hari yang lalu"/>
+        <YoutubeComp 
+            time="5.08" 
+            title="Tutorial React JS - Bagian 3" 
+            desc="11x ditonton, 8 hari yang lalu"/>
+        <YoutubeComp 
+            time="9.04" 
+            title="Tutorial React JS - Bagian 4" 
+            desc="5x ditonton, 4 hari yang lalu"/>
+        <YoutubeComp />
+      </Fragment>
+    );
+  }
+}
+
+YoutubeComp.defaultProps = {
+  time: "00.00",
+  title: "Title Here",
+  desc: "xx ditonton, x hari yang lalu"
+};
+
+export default Home;
