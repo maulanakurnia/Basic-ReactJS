ae5d8f15fb0d60e70cbeca54672c5233eb9d41d1
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-24 21:04:26 +0800

    add active style

--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+import {BrowserRouter, Route, NavLink} from 'react-router-dom'
 
 // pages
 import Product from "../pages/Product";
 import LifeCycleComp from "../pages/LifeCycleComp";
 import BlogPost from "../pages/BlogPost";
 import YoutubeCompPage from "../pages/YoutubeCompPage";
+import DetailPost from "../pages/BlogPost/DetailPost";
 
 // css
 import './Home.css'
@@ -19,12 +20,13 @@ class Home extends Component {
     return (
       <BrowserRouter>
         <div className="navigation">
-          <Link to="/">Blog</Link>
-          <Link to="/product">Product</Link>
-          <Link to="/lifeCycle">LifeCycle</Link>
-          <Link to="/youtube">Youtube</Link>
+          <NavLink exact to="/">Blog</NavLink>
+          <NavLink to="/product">Product</NavLink>
+          <NavLink to="/lifeCycle">LifeCycle</NavLink>
+          <NavLink to="/youtube">Youtube</NavLink>
         </div>
         <Route path="/" exact component={BlogPost}/>
+        <Route path="/detail-post/:id" component={DetailPost}/>
         <Route path="/product" component={Product}/>
         <Route path="/lifeCycle" component={LifeCycleComp}/>
         <Route path="/youtube" component={YoutubeCompPage}/>
