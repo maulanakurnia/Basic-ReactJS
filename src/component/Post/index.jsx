6c3491bd379eed8f8241b87a26227f0d4cfbc6ca
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-24 17:41:21 +0800

    API PUT

--- src/component/Post/index.jsx
+++ src/component/Post/index.jsx
@@ -10,6 +10,7 @@ export default (props) => {
                 <div className="content">
                     <p className="title">{props.data.title}</p>
                     <p className="desc">{props.data.body}</p>
+                    <button className="btn-update" onClick={() => props.update(props.data)}>Update</button>
                     <button className="btn-remove" onClick={() => props.remove(props.data.id)}>Remove</button>
                 </div>
             </div>
