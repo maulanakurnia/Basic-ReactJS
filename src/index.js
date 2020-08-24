7e8b93b893e11ff1d8a55275e7737d0b0b7c1ad1
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-25 07:24:26 +0800

    State Management with Context

--- src/index.js
+++ src/index.js
@@ -2,18 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './container/Home'
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-import rootReducer from './redux/reducer/globalReducer'
-
-// Store <-- Sebuah wadah besar yang menyimpan state secara global
-const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <Home/>
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
