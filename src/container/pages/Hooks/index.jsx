d1b2969830f62c79e606d2bfbc1eaf36f7fe5efa
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-25 11:07:45 +0800

    React Hook SideEffect

--- src/container/pages/Hooks/index.jsx
+++ src/container/pages/Hooks/index.jsx
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Hooks.css';
 
 const Hook = () => {
     const [count, setCount] = useState(0);
+
+    // useEffect memiliki kemampuan ComponentDidMount, ComponentDidUpdate, ComponentWillUnmount
+    useEffect(() => {
+        document.title = `Title Change ${count}`
+        return () => { // dan jika di dalam useEffect terdapat return maka, useEffect akan memanggil lifeCycle ComponentWillUnmount
+            document.title = `ReactJS Hello World!`
+        }
+    })
+
     return(
         <div className="p-hooks">
             <p>Nilai Saat ini adalah : {count}</p>
